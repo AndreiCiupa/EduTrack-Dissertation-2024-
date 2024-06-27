169d6e4c91be889cd9464c50ba8ab31396b4bbe1
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2024-06-27 03:44:24 +0300

    ok

---
+++ EduTrack/Models/StudentMarksViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EduTrack.Models
+{
+    internal class StudentMarksViewModel
+    {
+        public string StudentName { get; set; }
+        public double AverageMark { get; set; }
+        public List<Mark> Marks { get; set; }
+    }
+}
